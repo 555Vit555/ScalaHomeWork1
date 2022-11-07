66e10799acbfcb5cc8c8b3581ed4b27509b02f89
unknown <lvv@OGM2004.localnet.micran>
2022-11-07 17:26:01 +0700

    Initial commit

---
+++ src/main/scala/org/example/App.scala
@@ -0,0 +1,39 @@
+package org.example
+
+/**
+ * @author ${user.name}
+ */
+object App {
+  
+
+  
+  def main(args : Array[String]) {
+    //            Задание 3A
+    val first_str = "Hello, Scala!"
+    println(first_str)
+    val low_str = first_str.map(_.toLower)
+    println(low_str)
+    val remove_str = first_str.replace("!", "")
+    println(remove_str)
+    val second_str = (" and goodbye python!")
+    val result = remove_str + second_str
+    println(result)
+    //            Задание 3b
+    //import scala.io.StdIn.readLine
+    //println("Годовой доход:")
+    //val year_salary = readLine().toInt
+    //println("Премия:")
+    //val bonus = readLine().toFloat
+    //println("Компенсанция обеда:")
+    //val lunch = readLine().toInt
+    //val result_salary = (year_salary * bonus + lunch) * 0.87
+    //print(s"Годовой доход: $result_salary")
+    //            Задание 3c
+
+
+
+
+
+
+  }
+}
