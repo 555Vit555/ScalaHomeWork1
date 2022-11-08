91219cf870be5c723ac216fc2bad7559ab10de98
lvv <litvit555@gmail.com>
2022-11-08 17:01:44 +0700

    Home Work Scala 2.2 is ready

--- src/main/scala/org/example/App.scala
+++ src/main/scala/org/example/App.scala
@@ -16,15 +16,15 @@ object App {
     val result = remove_str + second_str
     println(result)
     //            Задание 3b
-    //import scala.io.StdIn.readLine
-    //println("Годовой доход:")
-    //val year_salary = readLine().toInt
-    //println("Премия:")
-    //val bonus = readLine().toFloat
-    //println("Компенсанция обеда:")
-    //val lunch = readLine().toInt
-    //val result_salary = (year_salary * bonus + lunch) * 0.87
-    //print(s"Годовой доход: $result_salary")
+    import scala.io.StdIn.readLine
+    println("Годовой доход:")
+    val year_salary = readLine().toInt
+    println("Премия:")
+    val bonus = readLine().toFloat
+    println("Компенсанция обеда:")
+    val lunch = readLine().toInt
+    val result_salary = (year_salary * bonus + lunch) * 0.87
+    println(s"Годовой доход: $result_salary")
     //            Задание 3c
     val firstList = List(100, 150, 200, 80, 120, 75)
     val commonSalary = firstList.sum
@@ -32,21 +32,56 @@ object App {
     val midSalary = commonSalary / countPerson
         var i = 0
     var printRez = 0
-    //while (i < countPerson) {
-      //printRez = 100 - (firstList(i) * 100 / midSalary)
-      //println(s"отклонение от средней з.п. = $printRez %")
-      //i = i + 1
+    while (i < countPerson) {
+      printRez = 100 - (firstList(i) * 100 / midSalary)
+      println(s"отклонение от средней з.п. = $printRez %")
+      i = i + 1
 
-    //}
+
+
+    }
     //                  Задание 3d
-    val secondList = firstList :+ 150
-    //val maxSalary = secondList.max
-    //val minSalary = secondList.min
-    //println(s"Максимальная з.п.:$maxSalary")
-    //println(s"Минимальная з.п.: $minSalary")
+    val secondList1 = firstList.dropRight(1)
+    val secondList = secondList1 :+ 125
+    println(secondList)
+    val maxSalary = secondList.max
+    val minSalary = secondList.min
+    println(s"Максимальная з.п.:$maxSalary")
+    println(s"Минимальная з.п.: $minSalary")
     //                  Задание 3e
-    //val thirdList = secondList :+ 350 :+ 90
-    //    println(thirdList.sorted)
+    val thirdList = (secondList :+ 350 :+ 90).sorted
+    println(s"Отсортированный список:$thirdList")
     //                  Задание 3f
+    val fourthList = (thirdList :+ 130).sorted
+    val Index = (fourthList indexOf 130) + 1
+    println(s"Новый сотрудник в списке:$Index")
+    //                  Задание 3g
+
+
+    val countFour = fourthList.size
+    println("Минимальная з.п. для middle:")
+    val minmidsalary = readLine().toInt
+    println("Максимальная з.п. для middle:")
+    val maxmidsalary = readLine().toInt
+
+    var j = 0
+    var printIndex = 0
+    while (j < countFour) {
+      if (minmidsalary < fourthList(j) && fourthList(j) < maxmidsalary)
+      {
+        var rend1 = fourthList(j)
+        var rend  =  fourthList indexOf rend1
+        println(s"Номер сотрудника middle $rend")
+        j = j + 1}
+      else {j = j + 1}
+    }
+    //                              Задание 3h
+    import scala.math._
+
+
+    var InndexList = fourthList.map(_ * 1.07)
+    println(s"Индексированная з.п. сотрудников: $InndexList ")
+
+
   }
 }
